390ac1051979baebed5f9ef0fa3399a88b73b11b
gabriellesote <gabrielle.sote@gmail.com>
2025-07-28 21:25:17 -0300

    finalizando conteudo de tuplas

---
+++ day02/Udemy/tuplas.py
@@ -0,0 +1,14 @@
+# tupla1 = (1,2,3)
+# print(tupla1)
+# tupla2 = (3,4,5,6)
+# print(tupla2)
+
+# print(tupla1 + tupla2)
+# print(tupla1)
+# print(tupla2)
+
+
+nome = 'gabrielle'
+tupla = tuple(nome)
+
+print(tupla.count('e'))
