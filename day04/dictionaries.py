b522fd3100606a7ea2781c800ce250c70118342a
gabriellesote <gabrielle.sote@gmail.com>
2025-08-04 17:33:16 -0300

    day004

---
+++ day04/dictionaries.py
@@ -0,0 +1,12 @@
+# paises = { 'br': 'Brasil', 'eua': 'Estados Unidos'}
+
+# print(paises)
+# paises2 = dict( br= 'Brasil', eua = 'Estados Unidos', py = 'Paraguay')
+# print(paises2)
+paises = { 'br': 'Brasil', 'eua': 'Estados Unidos'}
+print(paises['br'])
+print(paises['eua'])
+print(paises['br'])
+print(paises.get('br'))
+print(paises.get('ru'))
+print(paises.get('eua'))
